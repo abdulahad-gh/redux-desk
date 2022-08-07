13f88e8a2d8e0ff038be0361765ce743c89bc1c9
abdulahad-gh <abdulahadmailacc@gmail.com>
2022-08-07 13:55:32 +0600

    completed todo app

--- todos-app/src/component/Todos.js
+++ todos-app/src/component/Todos.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import todosAction from '../services/actions/todosAction';
 
 const Todos = () => {
+    const { todos, isLoading, error } = useSelector(state => state)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(todosAction())
+    }, [])
     return (
         <div>
             <h2>Todos App</h2>
+            {isLoading && <h3>Loading...</h3>}
+            {error && <h3 style={{ color: 'red' }}>{error}</h3>}
+            <section>
+
+                {
+                    todos && todos.map(todo => {
+                        const { id, title } = todo
+                        return <article key={id}>
+                            <h4>{id}</h4>
+                            <h4>{title}</h4>
+                        </article>
+
+                    })
+                }
+            </section>
+
 
         </div>
     );
