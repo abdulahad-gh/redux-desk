5c05c38b12f6bd1bdef1373cc195d224f3ec8ae2
abdulahad-gh <abdulahadmailacc@gmail.com>
2022-08-07 23:08:13 +0600

    create store

---
+++ redux-toolkit/src/app/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit/dist/configureStore"
+import counterReducer from "../features/counter/counterSlice"
+
+const store = configureStore({
+    reducer: {
+        counter: counterReducer
+    }
+})
+
+export default store;
