7906791a1f28ad2bcbe86cb1e615be6957829541
abdulahad-gh <abdulahadmailacc@gmail.com>
2022-08-07 23:11:09 +0600

    provide store

--- redux-toolkit/src/app/store.js
+++ redux-toolkit/src/app/store.js
@@ -1,4 +1,4 @@
-import { configureStore } from "@reduxjs/toolkit/dist/configureStore"
+import { configureStore } from "@reduxjs/toolkit"
 import counterReducer from "../features/counter/counterSlice"
 
 const store = configureStore({
