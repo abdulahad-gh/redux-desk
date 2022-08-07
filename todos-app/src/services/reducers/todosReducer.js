62193179be92d4754cfc6f6d9ae522363b0e9b2f
abdulahad-gh <abdulahadmailacc@gmail.com>
2022-08-07 13:08:13 +0600

    create todosReducer

---
+++ todos-app/src/services/reducers/todosReducer.js
@@ -0,0 +1,33 @@
+import { GET_FAILED, GET_REQUEST, GET_SUCCESS } from "../constants/todosConstants"
+
+
+const initState = {
+    todos: [],
+    isLoading: false,
+    error: null
+}
+
+const todosReducer = (state = initState, action) => {
+    switch (action.type) {
+        case GET_REQUEST:
+            return {
+                ...state,
+                isLoading: true
+            }
+        case GET_SUCCESS:
+            return {
+                isLoading: false,
+                todos: action.payload,
+                error: null
+            }
+        case GET_FAILED:
+            return {
+                isLoading: false,
+                todos: [],
+                error: action.payload
+            }
+        default:
+            return state
+    }
+
+}
